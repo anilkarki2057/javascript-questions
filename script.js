19d77cdae0598540c9fbcf448cd59c65f3f9672f
anilkarki2057 <anilkarki2057@gmail.com>
2023-09-14 15:18:38 +0545

    Day 1 of solving js questions

---
+++ script.js
@@ -0,0 +1,45 @@
+//1. Write a function that takes two numbers (a and b) as argument. Sum a and b. Return the result
+/*const sum = (a, b) => {
+  return a + b;
+};
+const result = sum(2, 3);
+console.log(result);
+
+//2.Write a function that takes two values, say a and b, as arguments.
+//Return true if the two values are equal and of the same type;
+const check = (a, b) => {
+  return a === b ? "True" : "False";
+};
+const result = check(2, 3);
+console.log(result);
+
+//3.Write a function that takes a value as argument. Return the type of the value.
+const checkType = (value) => {
+  return typeof value;
+};
+console.log(checkType(4));
+
+//4.Write a function that takes a string (a) and a number (n) as argument.
+//Return the nth character of 'a'.
+const myFunction = (string, number) => {
+  return string[number];
+};
+console.log(myFunction("abcde", 3));
+
+
+//5.Write a function that takes a string (a) as argument.
+// Remove the first 3 characters of a. Return the result
+
+const subString = (string) => {
+  return string.slice(2);
+};
+console.log(subString("hellow"));
+
+
+//6.Write a function that takes a string as argument.
+//Extract the last 3 characters from the string. Return the result.
+const subString = (string) => {
+  return string.slice(string.length - 3);
+};
+console.log(subString("abcdefgh"));
+*/
